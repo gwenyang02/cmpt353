5eebf481808894da0557ce62fb7c8b88466ea787
gwen <gwenyang02@gmail.com>
2024-10-27 00:20:21 -0700

    added getting all comments and submissions from distinct users

--- research.py
+++ research.py
@@ -2,6 +2,12 @@ import sys
 from pyspark.sql import SparkSession, functions, types
 
 spark = SparkSession.builder.appName('reddit user tracker for political subreddits').getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+# joining dataframes is slow: https://spark.apache.org/docs/latest/sql-performance-tuning.html
+spark.conf.set("spark.sql.adaptive.enabled",'True')
+spark.conf.set("spark.sql.adaptive.skewJoin.enabled",'True')
+assert sys.version_info >= (3,10) # make sure we have Python 3.10+
+assert spark.version >= '3.5' # make sure we have Spark 3.5+
 
 # Define paths
 reddit_submissions_path = '/courses/datasets/reddit_submissions_repartitioned/'
@@ -43,10 +49,14 @@ def main():
         'uspolitics', 'election2020', 'election2016', 'election2012', 'election2008',
         'conservative', 'democrats', 'republicans', 'libertarian', 'progressive'
     ]
+    # consider filtering years for smaller dataset
+    #filter_years = list(map(functions.lit, [2016,2020]))
+    election_subs = list(map(functions.lit, election_subs))
     
     # Filter comments from specified subreddits
     election_comments = reddit_comments \
         .where(reddit_comments['subreddit'].isin(election_subs)) \
+       # .where(reddit_submissions['year'].isin(filter_years)) \
         .select('author', 'id', 'body', 'subreddit', 'subreddit_id', 'year', 'month')
     
     # Save filtered election-related comments
@@ -55,23 +65,39 @@ def main():
     # Filter submissions from specified subreddits
     election_submissions = reddit_submissions \
         .where(reddit_submissions['subreddit'].isin(election_subs)) \
+       # .where(reddit_submissions['year'].isin(filter_years)) \
         .select('author', 'id', 'title', 'selftext', 'subreddit', 'subreddit_id', 'year', 'month', 'created')
     
     # Save filtered election-related submissions
     election_submissions.write.json(output + '/election_submissions', mode='overwrite', compression='gzip')
 
+    # based on distinct author in election subreddits,
+    # grab comments and submissions from an author with activity in election subreddits
+    # source: https://sparkbyexamples.com/pyspark/pyspark-select-distinct/
 
-    #based on distinct userid in election subreddits, 
-    # grab comments and submissions from all_comments by same userid
+    # select distinct authors who make posts and/or comment and extract 'author' column
+    # union with author ids in submissions
+    # then select distinct
+    election_authors = election_comments.select("author") \
+        .union(election_submissions.select("author")) \
+        .distinct() \
+        .cache()
 
-    #TO BE FILLED IN
+    # inner-join distinct author dataframe with all comments to filter for comments written by them in other subs
+    all_comments = reddit_comments.select('author', 'id', 'body', 'subreddit', 'subreddit_id', 'year', 'month') \
+                    #.where(reddit_submissions['year'].isin(filter_years)) \
+                    .join(broadcast(election_authors), on='author')
 
-    # # Save all comments for broader analysis
-    # all_comments = reddit_comments.select('author', 'id', 'body', 'subreddit', 'subreddit_id', 'year', 'month')
-    # all_comments.write.json(output + '/all_comments', mode='overwrite', compression='gzip')
+    # filter all submissions in same way as above
+    all_submissions = reddit_submissions.select('author', 'id', 'title', 'selftext', 'subreddit', 'subreddit_id',
+                                                'year', 'month', 'created') \
+                        #.where(reddit_submissions['year'].isin(filter_years)) \
+                        .join(broadcast(election_authors), on='author')
 
-    # # Save all submissions for broader analysis
-    # all_submissions = reddit_submissions.select('author', 'id', 'title', 'selftext', 'subreddit', 'subreddit_id', 'year', 'month', 'created')
-    # all_submissions.write.json(output + '/all_submissions', mode='overwrite', compression='gzip')
+    # save all comments as json
+    all_comments.write.json(output + '/all_comments', mode='overwrite', compression='gzip')
+    # save all submissions (posts) as json
+    all_submissions.write.json(output + '/all_submissions', mode='overwrite', compression='gzip')
 
+    # additional join filtering needed to find all comments and submissions for one user together
 main()
