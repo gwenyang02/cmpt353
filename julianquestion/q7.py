9886640326084d2579533137320e3fb147969d93
Julian Loewenherz <164665193+JulianLoewenherz@users.noreply.github.com>
2024-12-04 23:06:35 -0800

    made more changes

---
+++ julianquestion/q7.py
@@ -0,0 +1,28 @@
+# supervised ML
+
+
+# clustering with subsets of the data 
+
+
+# aggregated by user sentiment analysis x user aggregated activity on subreddits
+
+
+# potenitally interesting clustering if possible (not needed)
+
+
+# MOVING FORWARD USING CHAT TO GIVE LABELS THEN USING OUR SENTIMENT ANALYSIS AS A FEATURE COMBINED WITH ALL OUR OTHERS FOR CLASSIFICATION/PREDICTING CHAT LABEL
+#(also we can remove rows where chat thinks there is not an indicator of sentiment )
+
+
+#then we can actually do train test split on that 
+
+
+#do this for comments and titles
+
+
+# justification about using chatgpt explain our process
+
+
+
+
+#something else interesting we could do polynomial regression on it to predict magnitudes (food for thought not needed to do EXTRA)
